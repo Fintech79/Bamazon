d188c3823531c3ef0cdc72925853f5464fab056e
Fintech79 <abdelrahman.faisal@gmail.com>
2018-02-26 09:40:33 -0800

    add

---
+++ BamazonCustomer.js
@@ -0,0 +1,98 @@
+var mySql = require("mysql");
+
+var config = {
+	host: "localhost",
+	port: 3306,
+  // username
+  user: "root",
+   // user password
+   password: "admin",
+   database: "Bamazon"
+};
+var connection = mySql.createConnection(config);
+
+var callback = function(err){
+	if (err) throw err;
+	console.log('connection succesful');
+
+	displayItems();
+};
+connection.connect(callback)
+//displayItems();
+
+function displayItems(){
+	// assumes mysql connection made
+	connection.query("SELECT * FROM Bamazon", function (err, result) {
+    	if (err) throw err;
+    	console.log(result);
+	});// get items for sale from database
+	
+		//runSearch();
+}
+	
+function runSearch() {
+		var promise = inquirer.prompt({
+			name: "action",
+			type: "rawlist",
+			message: "What is your selection?",
+			choices: [
+			"Find product by id",
+			"How much quantitiy of the product would you like to purchase?",
+			]
+		});
+		// output for sale items
+		/*promise.then(function(result) {
+			/*switch (result.action) {
+				case "Find product by id":
+				productSearch();
+				break;
+
+				case "How much quantitiy of the product woulld you like to purchase?":
+				AddProduct();
+				break;
+
+
+
+			}
+
+		})
+		.catch(function(){
+			coconsole.log(err)
+		})
+		.finally(function(){
+			console.error('user didnt make a choice')
+		})*/
+}
+//function require inquierer
+
+function productSearch() {
+	inquirer
+	.prompt({
+		name: "product",
+		type: "input",
+		message: "What is your selection?"
+	})
+	/*.then(function(answer) {
+		var query = "SELECT * FROM products WHERE name= ? ",{name:product},function(err,response) {
+			for (var i = 0; i < res.length; i++) {
+				console.log("Position: " + res[i].product + " || productID: " + res[i]. + 
+					" || quantitiy: " + res[i].product);
+			}
+			runSearch();
+		};
+	});*/
+}
+
+function AddProduct() {
+	var query = "SELECT item FROM product WHERE count(*) > 1";
+	connection.query(query, function(err, res) {
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].product);
+		}
+		runSearch();
+	});
+}
+
+
+
+
---
+++ schema.sql
@@ -0,0 +1,55 @@
+ 
+DROP DATABASE IF EXISTS Products;
+
+CREATE DATABASE Products;
+
+-- Makes it so all of the following code will affect animals_db --
+USE Products;
+
+-- Creates the table "people" within animals_db --
+CREATE TABLE people (
+  item_id INTEGER(20) AUTO_INCREMENT NOT NULL,
+  -- Makes a string column called "name" which cannot contain null --
+  product_name VARCHAR(30) NOT NULL,
+  -- Makes a boolean column called "has_pet" which cannot contain null --
+  department_name VARCHAR (30)NOT NULL
+  -- Makes a sting column called "pet_name" --
+  price INTEGER (100)NOT NULL
+  -- Makes an numeric column called "pet_age" --
+  stock_quantity INTEGER (100)
+  PRIMARY KEY (id)
+);
+
+-
+
+ysql> insert into products (name,department,price,quantity)
+    -> values ("house","real estate",100000,2);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> select * from products;
++----+--------------+--------------+--------+----------+
+| id | name         | department   | price  | quantity |
++----+--------------+--------------+--------+----------+
+|  1 | boomerang    | sports       |     11 |       50 |
+|  2 | basketball   | sports       |     14 |      100 |
+|  3 | NFL2K        | video games  |     50 |       25 |
+|  4 | coffee maker | home         |     35 |       20 |
+|  5 | computer     | electronics  |    500 |       15 |
+|  6 | ring         | jewelery     |    999 |       13 |
+|  7 | BJJGI        | martial arts |    140 |       50 |
+|  8 | RayBans      | Apparel      |    100 |       65 |
+|  9 | car          | dealership   |    200 |        1 |
+| 10 | house        | real estate  | 100000 |        2 |
++----+--------------+--------------+--------+----------+
+10 rows in set (0.00 sec)
+
+
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '// Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database:' at line 1
+mysql>
+mysql>   password: admin
+    ->   p
